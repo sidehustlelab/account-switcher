554d65791a954bfbbee490b945c96072a060d92c
Anshul Ahuja <anshul.ahu@gmail.com>
2020-04-20 20:33:05 +0530

    Code cleanup

--- background.js
+++ background.js
@@ -1,33 +1,47 @@
-
-
-
 chrome.commands.onCommand.addListener(function (command) {
   if (command === 'update-url') {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
       var current_url = tabs[0].url;
       var account_num = 1;
+      var update_url_regex = null;
+      var update_acc = null;
 
       // Gmail
       const gmail_regex = "https:\/\/mail.google.com\/mail\/u\/[0-9].*";
       if (current_url.match(gmail_regex)) {
-        current_url = current_url.replace(RegExp("u\/[0-9]"), "u/" + account_num)
+        update_url_regex = RegExp("u\/[0-9]");
+        update_acc = "u/" + account_num
       }
+
       // Google Doc 
       const doc_regex = "https:\/\/docs.google.com\/document\/u\/[0-9].*";
       if (current_url.match(doc_regex)) {
-        current_url = current_url.replace(RegExp("u\/[0-9]"), "u/" + account_num)
+        update_url_regex = RegExp("u\/[0-9]");
+        update_acc = "u/" + account_num
       }
+
       // Google Drive 
       const drive_regex = "https:\/\/drive.google.com\/drive\/u\/[0-9].*";
       if (current_url.match(drive_regex)) {
-        current_url = current_url.replace(RegExp("u\/[0-9]"), "u/" + account_num)
+        update_url_regex = RegExp("u\/[0-9]");
+        update_acc = "u/" + account_num
       }
+
       // Google Play Music 
       const play_music_regex = "https:\/\/play.google.com\/music\/listen\\?u\=[0-9].*";
       if (current_url.match(play_music_regex)) {
-        current_url = current_url.replace(RegExp("\\?u\=[0-9]"), "?u=" + account_num)
+        update_url_regex = RegExp("\\?u\=[0-9]");
+        update_acc = "?u=" + account_num
       }
-      chrome.tabs.update({ url: current_url });
+
+      if (update_acc && update_url_regex) {
+        current_url = current_url.replace(update_url_regex, update_acc);
+        chrome.tabs.update({ url: current_url });
+      }
+      else {
+        console.log("Account switching is not supported on this page.")
+      }
+
     });
   }
 });
