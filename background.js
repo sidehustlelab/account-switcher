869703d6f35509c3ad4989152f93c54fa98838cf
Anshul Ahuja <anshul.ahu@gmail.com>
2020-04-20 20:22:28 +0530

    Working prototype for Gmail, drive, docs, play music for fixed acc

--- background.js
+++ background.js
@@ -1,35 +1,33 @@
 
 
+
 chrome.commands.onCommand.addListener(function (command) {
   if (command === 'update-url') {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-      let url = tabs[0].url;
-      console.log(url)
-      console.log("Triggered");
-      // URL Matching
+      var current_url = tabs[0].url;
+      var account_num = 1;
+
       // Gmail
       const gmail_regex = "https:\/\/mail.google.com\/mail\/u\/[0-9].*";
-      if(url.match(gmail_regex)){
-          console.log("GMAIL URL MATCH");
+      if (current_url.match(gmail_regex)) {
+        current_url = current_url.replace(RegExp("u\/[0-9]"), "u/" + account_num)
       }
       // Google Doc 
       const doc_regex = "https:\/\/docs.google.com\/document\/u\/[0-9].*";
-      if(url.match(doc_regex)){
-        console.log("DOC URL MATCH");
+      if (current_url.match(doc_regex)) {
+        current_url = current_url.replace(RegExp("u\/[0-9]"), "u/" + account_num)
       }
       // Google Drive 
       const drive_regex = "https:\/\/drive.google.com\/drive\/u\/[0-9].*";
-      if(url.match(drive_regex)){
-        console.log("GOOGLE DRIVE URL MATCH");
+      if (current_url.match(drive_regex)) {
+        current_url = current_url.replace(RegExp("u\/[0-9]"), "u/" + account_num)
       }
       // Google Play Music 
-      const play_music_regex = "https:\/\/play.google.com\/music\/listen\?u\=[0-9].*";
-      if(url.match(drive_regex)){
-        console.log("GOOGLE Play Music URL MATCH");
+      const play_music_regex = "https:\/\/play.google.com\/music\/listen\\?u\=[0-9].*";
+      if (current_url.match(play_music_regex)) {
+        current_url = current_url.replace(RegExp("\\?u\=[0-9]"), "?u=" + account_num)
       }
-    
-      chrome.tabs.update({ url: "https://www.google.com/" });
+      chrome.tabs.update({ url: current_url });
     });
   }
-  console.log(command);
 });
