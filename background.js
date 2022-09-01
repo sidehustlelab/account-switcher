df7991cb5ef5f5e905b3ccbb56223b9d7ee061bd
Jaeyong Kim <jaeyong@yshome.me>
2022-09-01 19:43:58 +0900

    feat: Generalized Google URL patterns to support more services (#19)
    * refactor: generalize url patterns

--- background.js
+++ background.js
@@ -5,119 +5,16 @@ chrome.runtime.onMessage.addListener(function (command) {
     var update_url_regex = null;
     var update_acc = null;
 
-    // Gmail
-    const gmail_regex = "https:\/\/mail.google.com\/mail\/u\/[0-9].*";
-    if (current_url.match(gmail_regex)) {
+    const google_regex_type1 = ".*.google.*\/u\/[0-9].*"
+    if (current_url.match(google_regex_type1)) {
       update_url_regex = RegExp("u\/[0-9]");
       update_acc = "u/" + account_num;
     }
 
-    // Google Docs
-    const doc_regex = "https:\/\/docs.google.com\/document\/u\/[0-9].*";
-    if (current_url.match(doc_regex)) {
-      update_url_regex = RegExp("u\/[0-9]");
-      update_acc = "u/" + account_num;
-    }
-
-    // Google Sheets
-    const sheets_regex = "https:\/\/docs.google.com\/spreadsheets\/u\/[0-9].*";
-    if (current_url.match(sheets_regex)) {
-      update_url_regex = RegExp("u\/[0-9]");
-      update_acc = "u/" + account_num;
-    }
-
-    // Google Slides
-    const slides_regex = "https:\/\/docs.google.com\/presentation\/u\/[0-9].*";
-    if (current_url.match(slides_regex)) {
-      update_url_regex = RegExp("u\/[0-9]");
-      update_acc = "u/" + account_num;
-    }
-
-    // Google Forms
-    const forms_regex = "https:\/\/docs.google.com\/forms\/u\/[0-9].*";
-    if (current_url.match(forms_regex)) {
-      update_url_regex = RegExp("u\/[0-9]");
-      update_acc = "u/" + account_num;
-    }
-
-    // Google Drive
-    const drive_regex = "https:\/\/drive.google.com\/drive\/u\/[0-9].*";
-    if (current_url.match(drive_regex)) {
-      update_url_regex = RegExp("u\/[0-9]");
-      update_acc = "u/" + account_num;
-    }
-
-    // Google Classroom
-    const classroom_regex = "https:\/\/classroom.google.com\/u\/[0-9].*";
-    if (current_url.match(classroom_regex)) {
-      update_url_regex = RegExp("u\/[0-9]");
-      update_acc = "u/" + account_num;
-    }
-
-    // Google Meet
-    const meet_regex = "https:\/\/meet.google.com\/.*";
-    if (current_url.match(meet_regex)) {
-      if (!current_url.match("authuser=[0-9]")) {
-        current_url = current_url + "?authuser=0";
-      }
+    const google_regex_type2 = ".*.google.*\?authuser=[0-9].*"
+    if (current_url.match(google_regex_type2)) {
       update_url_regex = RegExp("authuser=[0-9]");
       update_acc = "authuser=" + account_num;
-    }
-
-    // Google Hangouts
-    const hangouts_regex = "https:\/\/hangouts.google.com\/.*\?authuser=[0-9].*";
-    console.log(current_url, hangouts_regex);
-    if (current_url.match(hangouts_regex)) {
-      update_url_regex = RegExp("authuser=[0-9]");
-      update_acc = "authuser=" + account_num;
-    }
-
-    // Google Accounts
-    const accounts_regex = "https:\/\/myaccount.google.com\/u\/[0-9].*";
-    console.log(current_url, accounts_regex);
-    if (current_url.match(accounts_regex)) {
-      update_url_regex = RegExp("u\/[0-9]");
-      update_acc = "u/" + account_num;
-    }
-
-    // Google Duo web
-    const duo_regex = "https:\/\/duo.google.com\/u\/[0-9].*";
-    console.log(current_url, duo_regex);
-    if (current_url.match(duo_regex)) {
-      update_url_regex = RegExp("u\/[0-9]");
-      update_acc = "u/" + account_num;
-    }
-
-    // Google calendar
-    const calendar_regex = "https:\/\/calendar.google.com\/calendar\/u\/[0-9].*";
-    console.log(current_url, calendar_regex);
-    if (current_url.match(calendar_regex)) {
-      update_url_regex = RegExp("u\/[0-9]");
-      update_acc = "u/" + account_num;
-    }
-
-    // Google shopping
-    const shopping_regex = "https:\/\/www.google.co.in\/.*\?authuser=[0-9].*";
-    console.log(current_url, shopping_regex);
-    if (current_url.match(shopping_regex)) {
-      update_url_regex = RegExp("authuser=[0-9]");
-      update_acc = "authuser=" + account_num;
-    }
-
-    // Google translate
-    const translate_regex = "https:\/\/translate.google.co.in\/.*\?authuser=[0-9].*";
-    console.log(current_url, translate_regex);
-    if (current_url.match(translate_regex)) {
-      update_url_regex = RegExp("authuser=[0-9]");
-      update_acc = "authuser=" + account_num;
-    }
-
-    // Google Keep
-    const keep_regex = "https:\/\/keep.google.com\/u\/[0-9].*";
-    console.log(current_url, keep_regex);
-    if (current_url.match(keep_regex)) {
-      update_url_regex = RegExp("u\/[0-9]");
-      update_acc = "u/" + account_num;
     }
 
     if (update_acc && update_url_regex) {
